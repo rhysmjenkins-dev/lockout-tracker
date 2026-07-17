2f20a931bdf3da68daee3aad4eb7ee6e1a6d5104
rhysmjenkins-dev <rhysmjenkins@hotmail.com>
2026-07-17 16:17:19 +0100

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzcbiMfkq6D6PcySY2O-80NTHHmQplU0xzi1kzQG8OFuAYuw0F-YdI2IONkA3DVOhlH/exec',
-  CHART_COLORS: ['#667eea', '#f5576c', '#4facfe', '#00f2fe', '#fa709a'],
-  DEFAULT_FALSE_LOCKOUT_PENALTY: 10
-};
