93bdd312ce9f4dfa7a9bd3e7260ffa46620649cb
rhysmjenkins-dev <rhysmjenkins@hotmail.com>
2026-07-28 22:16:21 +0100

    Add files via upload

---
+++ theme-preview/config.js
@@ -0,0 +1,5 @@
+// Isolated beta Apps Script deployment. The production site uses its existing endpoint.
+window.LOCKOUT_CONFIG = Object.freeze({
+    apiUrl: 'https://script.google.com/macros/s/AKfycbwpR-oSSRkncpI_Nal2o57dFom6GeL5GnCCpk-X72DeK94XWgtYMt9bI6IVmDCl_Paj2w/exec',
+    version: '2.1-beta.10'
+});
